7d1d2a9e3e3575c9350aac0a63b4bed8c6209de9
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2022-11-05 14:34:09 +0300

    without git id

--- asm/asm3/asm3/main.cpp
+++ asm/asm3/asm3/main.cpp
@@ -18,168 +18,127 @@ uint16_t random() {
 }
 
 int main() {
-	unsigned short int data[512] = {0};
+	unsigned short int data[512] = { 0 };
 	int tempRandom = 0;
-	
+
 
 	__asm {
 		pushad
-	
-	    mov ebx, 0
-	    mov ecx, 0
+
+		mov ebx, 0
+		mov ecx, 0
 		lea edi, tempRandom
 
-	loopStart:
-<<<<<<< HEAD
-		xor esi, esi
-		lea esi, data
-=======
+		loopStart :
 
 		xor esi, esi
-		lea esi, data
+			lea esi, data
 
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
-		cmp bh, 127
-		je exitCycle
-		cmp bl, 128
-		je exitCycle
-		cmp ch, 128
-		je exitCycle
-		cmp cl, 128
-		je exitCycle
+			cmp bh, 127
+			je exitCycle
+			cmp bl, 128
+			je exitCycle
+			cmp ch, 128
+			je exitCycle
+			cmp cl, 128
+			je exitCycle
 
-		push ecx
-		call random
-<<<<<<< HEAD
-		pop ecx
-	    mov [edi], ax
-		
-=======
-		movzx eax, ax
-		pop ecx
-	    mov [edi], ax
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
+			push ecx
+			call random
+			movzx eax, ax
+			pop ecx
+			mov[edi], ax
 
-		test  eax, 1
-		jnz   odd			//Нечетное, переход на метку Odd
-		jz    evenNum		//Четное, переход на метку Even
+			test  eax, 1
+			jnz   odd
+			jz    evenNum
 
 
-	evenNum:
-<<<<<<< HEAD
-		adc bh, 1
-		mov eax, 2 //для вычисления сдвига
-=======
-		
-		cmp [esi], 0
-		je firstEven
+			evenNum :
 
-		adc bh, 1
-		mov eax, 2          //для вычисления сдвига
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
-		mul bh
-		adc esi, eax
-		mov eax, [edi]
-		mov [esi], eax
+		cmp[esi], 0
+			je firstEven
 
-		cmp eax, 50000
-		jae more50000
-		cmp eax, 10000
-		jbe less10000
-		jmp loopStart
+			adc bh, 1
+			mov eax, 2
+			mul bh
+			adc esi, eax
+			mov eax, [edi]
+			mov[esi], eax
 
-	odd:
-<<<<<<< HEAD
-		lea esi, data
-		adc bl, 1
-		mov eax, 2 //для вычисления сдвига
-=======
+			cmp eax, 50000
+			jae more50000
+			cmp eax, 10000
+			jbe less10000
+			jmp loopStart
+
+			odd :
 
 		adc bl, 1
-		mov eax, 2          //для вычисления сдвига
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
-		mul bl
-		adc esi, eax
-		mov eax, [edi]
-		mov[esi + 254], eax
+			mov eax, 2
+			mul bl
+			adc esi, eax
+			mov eax, [edi]
+			mov[esi + 254], eax
 
-		cmp eax, 50000
-		jae more50000
-		cmp eax, 10000
-		jbe less10000
-		jmp loopStart
+			cmp eax, 50000
+			jae more50000
+			cmp eax, 10000
+			jbe less10000
+			jmp loopStart
 
-	more50000:
-<<<<<<< HEAD
-		xor esi, esi
-		lea esi, data
-=======
+			more50000 :
 
 		xor esi, esi
-		lea esi, data
+			lea esi, data
 
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
-		adc ch, 1
-		mov eax, 2
-		mul ch
-		adc esi, eax
+			adc ch, 1
+			mov eax, 2
+			mul ch
+			adc esi, eax
 
-		mov eax, [edi]
-		mov[esi + 510], eax
+			mov eax, [edi]
+			mov[esi + 510], eax
 
-		jmp loopStart
+			jmp loopStart
 
-	less10000:
-<<<<<<< HEAD
-		xor esi, esi 
-		lea esi, data
-=======
-		
+			less10000 :
+
 		xor esi, esi
-		lea esi, data
+			lea esi, data
 
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
-		adc cl, 1
-		mov eax, 2
-		mul cl
-		adc esi, eax
+			adc cl, 1
+			mov eax, 2
+			mul cl
+			adc esi, eax
+
+			mov eax, [edi]
+			mov[esi + 766], eax
+
+			jmp loopStart
+
+			firstEven :
 
 		mov eax, [edi]
-		mov[esi + 766], eax
+			mov[esi], eax
 
-		jmp loopStart
+			cmp eax, 50000
+			jae more50000
+			cmp eax, 10000
+			jbe less10000
+			jmp loopStart
 
-	firstEven:
 
-		mov eax, [edi]
-		mov[esi], eax
-
-		cmp eax, 50000
-		jae more50000
-		cmp eax, 10000
-		jbe less10000
-		jmp loopStart
-	
-
-	exitCycle:
+			exitCycle :
 
 		popad
 	}
 
-<<<<<<< HEAD
-	cout << "Array\n";
-=======
 	cout << "Array(0 - 127 even, 127 - 255 odd, 255 - 383 more than 50000, 383 - 511 \nless than 10000):";
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
 	for (int i = 0; i < 512; i++) {
 		if (i % 128 == 0)
 			cout << "\n\n\n";
-		cout << data[i]<<"  ";
-<<<<<<< HEAD
-		if (i % 128 == 0)
-			cout << "\n\n\n";
-=======
->>>>>>> ac04179c5fe5d788412b913e535af28aa7e30ea3
+		cout << data[i] << "  ";
 	}
 	return 0;
 }
